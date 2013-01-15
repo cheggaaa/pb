2dde4d842fe0db5363d4235331e8bb9d9f2bd31c
Chris Howey <chris@howey.me>
2013-01-15 07:05:59 -0600

    Windows terminal width is now dynamic

--- pb_win.go
+++ pb_win.go
@@ -2,10 +2,17 @@
 
 package pb
 
+import (
+	"syscall"
+
+	"github.com/AllenDang/w32"
+)
+
 func bold(str string) string {
-    return str
+	return str
 }
 
 func terminalWidth() (int, error) {
-    return 79, nil
+	screenBufInfo := w32.GetConsoleScreenBufferInfo(w32.HANDLE(syscall.Stdout))
+	return int(screenBufInfo.DwSize.X)-1, nil
 }
