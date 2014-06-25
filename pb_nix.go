5460645578d453786e0ea8742e4e60fc7ad2dfee
Sergey Cherepanov <cheggaaa@gmail.com>
2014-06-25 12:59:47 +0400

    Solaris support

--- pb_nix.go
+++ pb_nix.go
@@ -11,6 +11,8 @@ import (
 const (
 	TIOCGWINSZ     = 0x5413
 	TIOCGWINSZ_OSX = 1074295912
+
+	SYS_IOCTL_SOLARIS = 54
 )
 
 func bold(str string) string {
@@ -20,10 +22,17 @@ func bold(str string) string {
 func terminalWidth() (int, error) {
 	w := new(window)
 	tio := syscall.TIOCGWINSZ
-	if runtime.GOOS == "darwin" {
+	sys_ioctl := syscall.SYS_IOCTL
+	switch runtime.GOOS {
+	case "darwin":
 		tio = TIOCGWINSZ_OSX
+		break
+	case "solaris":
+		sys_ioctl = SYS_IOCTL_SOLARIS
+		break
 	}
-	res, _, err := syscall.Syscall(syscall.SYS_IOCTL,
+
+	res, _, err := syscall.Syscall(uintptr(sys_ioctl),
 		uintptr(syscall.Stdin),
 		uintptr(tio),
 		uintptr(unsafe.Pointer(w)),
