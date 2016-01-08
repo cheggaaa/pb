cc46b02092687616f236433896b16192d3e71835
Sergey Cherepanov <cheggaaa@gmail.com>
2016-01-08 14:52:19 +0300

    TCGETS TCSETS from syscall

--- pb_x.go
+++ pb_x.go
@@ -18,11 +18,8 @@ const (
 	TIOCGWINSZ_OSX = 1074295912
 )
 
-// These constants are declared here, rather than importing
-// them from the syscall package as some syscall packages, even
-// on linux, for example gccgo, do not declare them.
-const ioctlReadTermios = 0x5401  // syscall.TCGETS
-const ioctlWriteTermios = 0x5402 // syscall.TCSETS
+const ioctlReadTermios = syscall.TCGETS
+const ioctlWriteTermios = syscall.TCSETS
 
 var tty *os.File
 
