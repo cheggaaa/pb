a6373abe963a383a39805dce974f6f4d26539ebc
Sergey Cherepanov <cheggaaa@gmail.com>
2017-03-06 13:45:01 +0300

    format_test to 'pb' package

--- format_test.go
+++ format_test.go
@@ -1,18 +1,16 @@
-package pb_test
+package pb
 
 import (
 	"fmt"
 	"strconv"
 	"testing"
 	"time"
-
-	"gopkg.in/cheggaaa/pb.v1"
 )
 
 func Test_DefaultsToInteger(t *testing.T) {
 	value := int64(1000)
 	expected := strconv.Itoa(int(value))
-	actual := pb.Format(value).String()
+	actual := Format(value).String()
 
 	if actual != expected {
 		t.Error(fmt.Sprintf("Expected {%s} was {%s}", expected, actual))
@@ -22,7 +20,7 @@ func Test_DefaultsToInteger(t *testing.T) {
 func Test_CanFormatAsInteger(t *testing.T) {
 	value := int64(1000)
 	expected := strconv.Itoa(int(value))
-	actual := pb.Format(value).To(pb.U_NO).String()
+	actual := Format(value).To(U_NO).String()
 
 	if actual != expected {
 		t.Error(fmt.Sprintf("Expected {%s} was {%s}", expected, actual))
@@ -36,13 +34,13 @@ func Test_CanFormatAsBytes(t *testing.T) {
 	}{
 		{v: 1000, e: "1000 B"},
 		{v: 1024, e: "1.00 KiB"},
-		{v: 3*pb.MiB + 140*pb.KiB, e: "3.14 MiB"},
-		{v: 2 * pb.GiB, e: "2.00 GiB"},
-		{v: 2048 * pb.GiB, e: "2.00 TiB"},
+		{v: 3*MiB + 140*KiB, e: "3.14 MiB"},
+		{v: 2 * GiB, e: "2.00 GiB"},
+		{v: 2048 * GiB, e: "2.00 TiB"},
 	}
 
 	for _, input := range inputs {
-		actual := pb.Format(input.v).To(pb.U_BYTES).String()
+		actual := Format(input.v).To(U_BYTES).String()
 		if actual != input.e {
 			t.Error(fmt.Sprintf("Expected {%s} was {%s}", input.e, actual))
 		}
@@ -52,7 +50,7 @@ func Test_CanFormatAsBytes(t *testing.T) {
 func Test_CanFormatDuration(t *testing.T) {
 	value := 10 * time.Minute
 	expected := "10m0s"
-	actual := pb.Format(int64(value)).To(pb.U_DURATION).String()
+	actual := Format(int64(value)).To(U_DURATION).String()
 	if actual != expected {
 		t.Error(fmt.Sprintf("Expected {%s} was {%s}", expected, actual))
 	}
@@ -61,7 +59,7 @@ func Test_CanFormatDuration(t *testing.T) {
 func Test_DefaultUnitsWidth(t *testing.T) {
 	value := 10
 	expected := "     10"
-	actual := pb.Format(int64(value)).Width(7).String()
+	actual := Format(int64(value)).Width(7).String()
 	if actual != expected {
 		t.Error(fmt.Sprintf("Expected {%s} was {%s}", expected, actual))
 	}
