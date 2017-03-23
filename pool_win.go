b70adc94d6d9454331a94dc3c5fee1858a38959b
Ian Compton <ian.a.compton@googlemail.com>
2017-03-23 10:14:09 +0000

    Update pool_win.go

--- pool_win.go
+++ pool_win.go
@@ -30,7 +30,7 @@ func (p *Pool) print(first bool) bool {
 		bar.Update()
 		out += fmt.Sprintf("\r%s\n", bar.String())
 	}
-	if p.Output {
+	if p.Output != nil {
 		fmt.Fprint(p.Output, out)
 	} else {
 		fmt.Print(out)
