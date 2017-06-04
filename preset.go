ae83553e2f707aad21e293aaaf9816b3dd6dcc27
Sergey Cherepanov <cheggaaa@gmail.com>
2017-06-04 18:03:45 +0300

    fix preset

--- preset.go
+++ preset.go
@@ -4,8 +4,8 @@ import ()
 
 var (
 	// Full - preset with all default available elements
-	// Example: 'Prefix 20/100 20s [-->______] 20% 1 p/s ETA 1m Suffix'
-	Full ProgressBarTemplate = `{{string . "prefix"}}{{counters . }} {{etime . }} {{bar . }} {{percent . }} {{speed . }} {{rtime . }}{{string . "suffix"}}`
+	// Example: 'Prefix 20/100 [-->______] 20% 1 p/s ETA 1m Suffix'
+	Full ProgressBarTemplate = `{{string . "prefix"}}{{counters . }} {{bar . }} {{percent . }} {{speed . }} {{rtime . "ETA %s"}}{{string . "suffix"}}`
 
 	// Default - preset like Full but without elapsed time
 	// Example: 'Prefix 20/100 [-->______] 20% 1 p/s ETA 1m Suffix'
